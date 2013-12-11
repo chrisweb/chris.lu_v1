5455fe854c5ad8e930422050edd2f4158df7f866
Chris Weber <chris963@gmail.com>
2013-12-11 20:33:04 +0100

    google analytics code update for gender and age tracking

--- application/layouts/helpers/GoogleAnalytics.php
+++ application/layouts/helpers/GoogleAnalytics.php
@@ -13,12 +13,23 @@ class Application_Layouts_Helpers_GoogleAnalytics extends Zend_View_Helper_Abstr
         if (APPLICATION_ENV === 'production' && $controllerName !== 'admin' && $actionName !== 'login')
 		{
 
-			$output = "<script>
+			/*$output = "<script>
 					var _gaq=[['_setAccount','UA-16705563-1'],['_trackPageview']];
 					(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 					g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 					s.parentNode.insertBefore(g,s)}(document,'script'));
-				</script>";
+				</script>";*/
+
+			$output = "<script>
+                var _gaq = _gaq || [];
+                _gaq.push(['_setAccount', 'UA-16705563-1']);
+                _gaq.push(['_trackPageview']);
+                (function() {
+                    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                })();
+                </script>";
 
         } else {
 
